a990f4a776423143fbf4a77dd91e743faeec0093
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2024-01-22 23:24:54 +0530

    Add files via upload

---
+++ Third.java
@@ -0,0 +1,12 @@
+public class Third {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 5;
+        int num3 = 5;
+        int sum = num1 + num2 + num3;
+        int average = (sum)/3;
+        System.out.println("Sum is : " +sum);
+        System.out.println("Average is : " +average);
+
+    }
+}
