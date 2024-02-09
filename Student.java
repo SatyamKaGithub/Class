ceaaa944d600117b5fc04b5d0787df63ed625dab
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2024-02-09 11:07:55 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    public static void main(String[] args) {
+        String r = "2201020609";
+        String n = "Satyam kumar";
+        String b = "CSE(WT)";
+        String c = "CGU";
+        System.out.println("Registration number : " +r);
+        System.out.println("Student name : " +n);
+        System.out.println("Branch : " +b);
+        System.out.println("University : " +c);
+
+
+    }
+}
