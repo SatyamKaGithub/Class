42e840e0d9589eed88c7d7213829c24a0fb798a2
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2024-02-09 11:06:22 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,18 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 20;
+        System.out.println("Given number :" +a+ ", " +b);
+        int c = a + b;
+        System.out.println("Summation : " +c);
+        int d = a -b;
+        System.out.println("Subtraction :" +d);
+        int e = a * b;
+        System.out.println("Product : " +e);
+        int f = a / b;
+        System.out.println("Divide : " +f);
+        int g = a % b;
+        System.out.println("Mod: " +g );
+
+    }
+}
