7c40d6455d7a6ac4820690a1312678db3a61a63d
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2024-02-09 11:06:52 +0530

    Add files via upload

---
+++ Float.java
@@ -0,0 +1,7 @@
+public class Float {
+    public static void main(String[] args) {
+        float x = 5.2f;
+        float y = 3.6f;
+        System.out.println("Float value: " +x+ "," +y);
+    }
+}
