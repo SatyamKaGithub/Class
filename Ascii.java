3630a7c7737ff0820213e4f9178df01b84c6af42
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2024-02-09 11:06:38 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,8 @@
+public class Ascii {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int n = 100;
+        System.out.println("ASCII value of character = " +(int)ch);
+        System.out.println("ASCII value of integer = "  +(char)n);
+    }
+}
