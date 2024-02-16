c0f3de12ef9e8f4269d5798f9b7863fa93169670
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2024-02-16 11:54:35 +0530

    Add files via upload

---
+++ P3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class P3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n, f0=0,f1=1,fib,i;
+        System.out.println("Enter the range value : ");
+        n = in.nextInt();
+        System.out.println(f0);
+        System.out.println(f1);
+    }
+}
