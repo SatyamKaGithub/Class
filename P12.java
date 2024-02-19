7d99392e5e96fb344b7bc4171966117a04c10991
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2024-02-19 21:06:08 +0530

    Add files via upload

---
+++ P12.java
@@ -0,0 +1,9 @@
+// Create a class date to display current date without any member method
+import java.util.Date;
+
+public class P12 {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Current Date: " + currentDate);
+    }
+}
